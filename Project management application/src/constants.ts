d978080fa188f2b968ee72554e01ccfaa17544b0
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-12-03 19:54:19 +0300

    refactor: split connection string onto constants

--- Project management application/src/constants.ts
+++ Project management application/src/constants.ts
@@ -1,2 +1,7 @@
 export const PORT = 3001;
 export const SECRET_KEY = '86b0e8db4c6eeb151ce5ce2e23cfd967';
+export const CLUSTER = 'cluster0.gthojfx';
+export const USERNAME = 'rssreactapp';
+export const PASSWORD = 'SSl9WzNchug21Ykj';
+export const DB_NAME = 'managerApp';
+export const DB_PARAMS = '?retryWrites=true&w=majority';
--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
-import { PORT } from './constants';
+import { CLUSTER, DB_NAME, DB_PARAMS, PASSWORD, PORT, USERNAME } from './constants';
 
 import * as serverService from './services/server.service';
 
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://rssreactapp:SSl9WzNchug21Ykj@cluster0.gthojfx.mongodb.net/managerApp?retryWrites=true&w=majority');
+    await mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER}.mongodb.net/${DB_NAME}${DB_PARAMS}`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
